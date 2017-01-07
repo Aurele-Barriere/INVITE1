705db6468fad735270a54e07a72477842f78ed63
Jérémy Thibault <jeremy.thibault@ens-rennes.fr>
2017-01-07 23:46:52 +0100

    svr with gradient

--- main.m
+++ main.m
@@ -8,7 +8,9 @@ addpath('svr');
 
 imshow(images_test_lr(1:10, 1:10, 1:1), []);
 
-res = super_resolution(images_apprentissage_lr(1:10, 1:10, 1:2), images_apprentissage_hr(1:20, 1:20, 1:2), images_test_lr(1:10, 1:10, 1:1));
+params.method = 2
+
+res = super_resolution(images_apprentissage_lr(1:10, 1:10, 1:5), images_apprentissage_hr(1:20, 1:20, 1:5), images_test_lr(1:10, 1:10, 1:1), params);
 
 disp(size(res));
 imshow(res, []);
--- super_resolution.m
+++ super_resolution.m
@@ -8,7 +8,7 @@ function images_superresolues = super_resolution(images_learning_basse_resolutio
 [s1_, s2_, s3_] = size(images_learning_haute_resolution);
 assert (s3 == s3_);
 
-if (nargin == 3 || exists(param.R) == 0)
+if (nargin == 3 || isfield(param, 'R') == 0)
   R = 1;
 else
   R=param.R;
@@ -35,31 +35,13 @@ im_sortie = transpose(im_sortie);
 % SVR method
 if(nargin == 3 || (param.method == 0 && param.gamma == 0))
     
-    % cross-validation
     nb_folds = 10;
     [label_kt,label_kv,data_kt,data_kv] = decoupe_data(im_entree, im_sortie, nb_folds);
-    perf_moy = [0 0 0 0 0 0 0 0];
     gamma=[0.1 0.5 1 2 5 10 15 20];
-    for g = 1:8
-        options.kernel_d = gamma(g);
-        options.kernel_type = 'gaussian';
-        for i=1:nb_folds
-            X = label_kv{i};
-            X_ = svm_regression(data_kv{i}, label_kt{i}, data_kt{i}, options);
-            [x1 x2] = size(X);
-            perf = sqrt ( (1 / (x1*x2)) * norm (X - X_) * norm (X - X_));
-            perf_moy(g) = perf_moy(g) + perf;
-        end
-    end
-    perf_moy = perf_moy ./ nb_folds
     
-    imin = 1;
-    for i = 1:8
-        if (perf_moy(i) < perf_moy(imin))
-            imin = i;
-        end
-    end
-    param.gamma = gamma(imin);
+    % does the cross validation
+    best = cross_validation(nb_folds, label_kt, label_kv, data_kt, data_kv, gamma)
+    param.gamma = best;
        
 end
 
@@ -77,30 +59,102 @@ if (nargin == 3 || param.method == 0)
     disp("images tests   ");disp(size(im_test));
     disp(im_test); fflush(stdout);
     %}
-    images_superresolues = svm_regression(im_test, im_sortie, im_entree, options);
+    im_sup = svm_regression(im_test, im_sortie, im_entree, options);
     %{
     disp("images superresolues   ");disp(size(images_superresolues));
     disp(images_superresolues); fflush(stdout);
     %}
+    
+    
     % reshape
-    disp(size(images_superresolues))
     image_res = zeros(t1*R_,t2*R_);
     for i = 1:t1
       for j = 1:t2
         for t = 1:R_
           for t_ = 1:R_
-            image_res(R_*(i-1)+t, R_*(j-1)+t_) = images_superresolues(i + t1*(j-1), t+R_*(t_-1));
+            image_res(R_*(i-1)+t, R_*(j-1)+t_) = im_sup(i + t1*(j-1), t+R_*(t_-1));
           end
         end
       end
     end
-    disp(size(image_res))
-    images_superresolues = image_res
+    disp(size(image_res));
+    images_superresolues = image_res;
     % images_superresolues_ = reshape(images_superresolues, [R_*t1 R_*t2]);
     
 end
-%}
 
+if (nargin == 4 && param.method == 2)
+  param.sigma = [2 3 5 10 20];
+  
+  for sigma = param.sigma
+    new_line_x = [];
+    new_line_y = [];
+    for t = 1:s3
+      [im_entree_x, im_entree_y] = gradient_images(images_learning_basse_resolution(:,:,t), sigma);
+      for i = 1:s1
+        for j = 1:s2
+          new_line_x = [new_line_x im_entree_x(i,j)];
+          new_line_y = [new_line_y im_entree_y(i,j)];
+        end
+      end
+    end
+    im_entree = [im_entree transpose(new_line_x) transpose(new_line_y)];
+  end
+  
+  
+  % cross validation
+  if (not(isfield(param, 'gamma')) || param.gamma == 0)
+    nb_folds = 10;
+    [label_kt,label_kv,data_kt,data_kv] = decoupe_data(im_entree, im_sortie, nb_folds);
+    gamma=[0.1 0.5 1 2 5 10 15 20];
+    best = cross_validation(nb_folds, label_kt, label_kv, data_kt, data_kv, gamma);
+    param.gamma = best;
+  end
+  
+  options.kernel_d = param.gamma;
+  options.kernel_type = 'gaussian';
+
+  % the end
+  
+  % prepare the test image
+  im_test = [];
+  [t1 t2 t3] = size(images_test);
+  for n = 1:1 % 1:t3 for all images
+    im_test = [im_test; im2patches(images_test(:,:,n),R,'replicate')];
+  end
+  for sigma = param.sigma
+    new_line_x = [];
+    new_line_y = [];
+    for t = 1:t3
+      [im_test_x, im_test_y] = gradient_images(images_learning_basse_resolution(:,:,t), sigma);
+      for i = 1:t1
+        for j = 1:t2
+          new_line_x = [new_line_x im_test_x(i,j)];
+          new_line_y = [new_line_y im_test_y(i,j)];
+        end
+      end
+    end
+    im_test = [im_test transpose(new_line_x) transpose(new_line_y)];
+  end
+  disp(size(im_test)); disp(size(im_sortie)); disp(size(im_entree));
+  
+  
+  im_sup = svm_regression(im_test, im_sortie, im_entree, options);
+  image_res = zeros(t1*R_,t2*R_);
+    for i = 1:t1
+      for j = 1:t2
+        for t = 1:R_
+          for t_ = 1:R_
+            image_res(R_*(i-1)+t, R_*(j-1)+t_) = im_sup(i + t1*(j-1), t+R_*(t_-1));
+          end
+        end
+      end
+    end
+  disp(size(image_res));
+  images_superresolues = image_res;
+  
+  
+end
 
 % test with
 % super_resolution(images_apprentissage_lr(100:120, 100:120, 1:2), images_apprentissage_hr(100:120, 100:120, 1:2), images_test_lr)
